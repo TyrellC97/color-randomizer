d60b643c2935621d6ff1c2ab11e2887040e29889
tyrellc97 <tyrellcoleman97@gmail.com>
2024-02-28 15:48:32 -0500

    past colors

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ var container = document.querySelector(".container");
 var colorCard = document.querySelector(".color-card");
 var btn = document.querySelector("button");
 var hex = document.querySelector("span");
+var list = document.getElementById("list")
+let liItem = document.createElement("li")
+
 
 function getNewColor() {
     var symbols = '0123456789ABCDEF'
     var color = "#"
+    let liItem = document.createElement("li")
 
     for (var i = 0; i < 6; i++) {
         color = color + symbols[Math.floor(Math.random() * 16)];
@@ -14,6 +18,14 @@ function getNewColor() {
     document.body.style.background = color
     colorCard.style.background = color
     hex.innerHTML = color
+
+    list.appendChild(liItem)
+    liItem.innerHTML = color
 }
 btn.addEventListener("click", getNewColor);
+
+
+
+
+
 
